3b24209c244c2098fe91697a12b9c30fb6891ff3
Philippe Virouleau <philippe.44@gmail.com>
2017-06-28 16:21:34 +0200

    Add missing "fake" registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -57,6 +57,16 @@ class RegistrationsController < ApplicationController
         r.details.staff = true
         r.details.save!(validate: false)
       end
+      # Create missing registrations/account for badges
+      User.create(id: 27324, name: "Andrea Chana", country_iso2: "ES")
+      # Fake user id, to update if she gets an account
+      User.create(id: 300000, name: "Silvia Bubendorf", country_iso2: "CH")
+      User.create(id: 29176, name: "Aurelien Souchet (高凡)", country_iso2: "FR", wca_id: "2006SOUC01")
+      [22653, 27324, 300000, 29176].each do |uid|
+        registration = Registration.create(user_id: uid, event_ids: "", status: "deleted")
+        registration.details.staff = true
+        registration.save!(validate: false)
+      end
     end
 
     redirect_to(registrations_url, flash: { success: "Imported #{all_registrations.size} registrations and users successfully!" })
