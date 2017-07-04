108e122c043bae5aeba08e76ce882f910663d805
Philippe Virouleau <philippe.44@gmail.com>
2017-07-04 23:01:26 +0200

    Fix schedules

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -82,16 +82,17 @@ class RegistrationsController < ApplicationController
   end
 
   def printable_schedules
-    @registrations = Registration.includes([registration_detail: [], scramble_events: [], user: [], groups: [:round], staff_registrations_groups: { group: [:round] }, staff_teams_groups: { group: [:round] }]).order(:id)
+    @registrations = Registration.includes([registration_detail: [], scramble_events: [], user: [], groups: [:round], staff_registrations_groups: { group: [:round] }, staff_teams_groups: { group: [:round] }])
     @side_event = ["333mbf", "444bf", "555bf"]
-    length = 500
+    length = 30
     offset = 0
     case params[:type]
     when "staff"
-      @registrations = @registrations.staff_available.shuffle
+      @registrations = @registrations.staff_available.sort_by(&:name)
     else
       offset = params[:off]&.to_i || 0
-      @registrations = @registrations.accepted.limit(length).offset(offset).reject { |r| r.staff? }
+      @registrations = @registrations.accepted.reject { |r| r.staff? }.sort_by(&:name)
+      @registrations = @registrations.slice!(offset, length)
     end
   end
 
