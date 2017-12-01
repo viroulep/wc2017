c6cb1350fae7f6c0f10bd5366a44d466c5eb966c
Philippe Virouleau <philippe.44@gmail.com>
2017-12-01 20:38:38 +0100

    Prep for Italian Championship

--- config/initializers/default_environment.rb
+++ config/initializers/default_environment.rb
@@ -3,6 +3,6 @@
 # FIXME: change this to the prod url when we're live
 ENV["WCA_CALLBACK_URL"] ||= "http://127.0.0.1:3000/wca_callback"
 ENV["WCA_BASE_URL"] ||= "https://www.worldcubeassociation.org"
-ENV["WCA_COMP_ID"] ||= "ItalianOpen2017"
+ENV["WCA_COMP_ID"] ||= "ItalianChampionship2017"
 ENV["GROUPS_VISIBLE"] ||= "true"
 ENV["ANON_PASS"] ||= ""
