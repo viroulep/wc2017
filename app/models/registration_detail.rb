a9bc39e43ac7edbd710072428f0a308a06e723cc
zeecho <zeecho@users.noreply.github.com>
2018-02-21 23:33:27 +0100

    Use custom validate for tshirt

--- app/models/registration_detail.rb
+++ app/models/registration_detail.rb
@@ -3,6 +3,16 @@ class RegistrationDetail < ApplicationRecord
 
   TSHIRT_SIZES = %w(XS S M L XL XXL XXXL).freeze
 #  validates_inclusion_of :tshirt, in: TSHIRT_SIZES, message: "%{value} is not a valid size"
+  validate :is_a_size_or_blank
+
+  private def is_a_size_or_blank
+    if !TSHIRT_SIZES.include?(tshirt)
+      unless tshirt.blank?
+        errors.add(:tshirt, "Not a valid size")
+      end
+    end
+  end
+
   validates_inclusion_of :staff, in: [true, false]
 
   validates_numericality_of :mbf1, greater_than_or_equal_to: 0
