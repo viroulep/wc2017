5104f5a5e5293634d061f004f5c4dc2718ad5732
Philippe Virouleau <philippe.44@gmail.com>
2018-06-18 23:46:55 +0200

    access to schedule .json

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:schedule]
   before_action :redirect_unless_admin!, except: [:edit, :update, :confirm, :schedule]
   before_action :redirect_unless_can_edit!, only: [:edit, :update, :confirm]
   before_action :redirect_unless_can_view_groups!, only: [:schedule]
