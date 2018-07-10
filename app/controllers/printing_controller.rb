e54436ae528bfeb13d041f526b2c2cb7581717fd
Philippe Virouleau <philippe.44@gmail.com>
2018-07-10 16:49:41 +0200

    update printable stuff

--- app/controllers/printing_controller.rb
+++ app/controllers/printing_controller.rb
@@ -5,7 +5,7 @@ class PrintingController < ApplicationController
   def printable_schedules
     @registrations = Registration.includes([registration_detail: [], scramble_events: [], user: [], groups: [:round], staff_registrations_groups: { group: [:round] }, staff_teams_groups: { group: [:round] }])
     @side_event = ["333mbf", "444bf", "555bf"]
-    length = 700
+    length = 10
     offset = 0
     case params[:type]
     when "staff"
